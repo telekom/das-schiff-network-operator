71a2f958f88bbd26c347669239f362d49c27810a
christopher.dziomba@telekom.de <christopher.dziomba@telekom.de>
2023-07-05 15:13:21 +0200

    Formatting

--- pkg/nl/layer2.go
+++ pkg/nl/layer2.go
@@ -65,6 +65,7 @@ func (n *NetlinkManager) CreateL2(info Layer2Information) error {
 	if err := n.configureBridge(bridge.Name); err != nil {
 		return err
 	}
+
 	// Wait 500ms before configuring anycast gateways on newly added interface
 	time.Sleep(500 * time.Millisecond)
 	for _, addr := range info.AnycastGateways {
