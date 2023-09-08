1db69a04bea3c6d2ab154b062d517d9626f4aeab
Marcel Fest <marcel.fest@telekom.de>
2023-09-08 07:24:51 +0200

    Use a different TaintKey as kubelet does not support dual-stack for external cloud-providers

--- pkg/healthcheck/healthcheck.go
+++ pkg/healthcheck/healthcheck.go
@@ -27,7 +27,12 @@ const (
 	// NodenameEnv is an env variable that holds Kubernetes node's name.
 	NodenameEnv = "NODE_NAME"
 	// TaintKey is a node's CPI taint key that causes NoSchedule effect.
-	TaintKey = "node.cloudprovider.kubernetes.io/uninitialized"
+	//
+	// TaintKey = "node.cloudprovider.kubernetes.io/uninitialized"
+	// we need to use our own TaintKey as dual-stack is not supported
+	// for external cloud-providers in kubernetes until 1.27.x.
+	// ref: https://kubernetes.io/docs/concepts/services-networking/dual-stack/#configure-ipv4-ipv6-dual-stack
+	TaintKey = "node.schiff.telekom.de/uninitialized"
 
 	configEnv         = "OPERATOR_NETHEALTHCHECK_CONFIG"
 	defaultTCPTimeout = 3
