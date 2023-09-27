73189472f504fe3330e16a007a1003e82c02afe8
Marcel Fest <marcel.fest@telekom.de>
2023-09-27 18:20:30 +0200

    Fix G114 sec issue

--- cmd/frr-monitoring/main.go
+++ cmd/frr-monitoring/main.go
@@ -1,20 +1,3 @@
-// Copyright 2022 The Prometheus Authors
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-//go:build go1.17
-// +build go1.17
-
-// A minimal example of how to include Prometheus instrumentation.
 package main
 
 import (
@@ -22,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
@@ -29,12 +13,15 @@ import (
 	"github.com/telekom/das-schiff-network-operator/pkg/monitoring"
 )
 
+const (
+	twenty = 20
+)
+
 var (
 	addr = flag.String("listen-address", ":7082", "The address to listen on for HTTP requests.")
 )
 
 func main() {
-
 	flag.Parse()
 
 	// Create a new registry.
@@ -59,7 +46,13 @@ func main() {
 		promhttp.HandlerOpts{
 			// Opt into OpenMetrics to support exemplars.
 			EnableOpenMetrics: true,
+			Timeout:           time.Minute,
 		},
 	))
-	log.Fatal(http.ListenAndServe(*addr, nil))
+	server := http.Server{
+		Addr:              *addr,
+		ReadHeaderTimeout: twenty * time.Second,
+		ReadTimeout:       time.Minute,
+	}
+	log.Fatal(server.ListenAndServe())
 }
