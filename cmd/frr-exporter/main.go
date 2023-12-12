c605cf76acc88031e5de21535b5037b9b132a5ca
Marcel Fest <marcel.fest@telekom.de>
2023-12-12 11:38:13 +0100

    Fixed frr-exporter termination hang

--- cmd/frr-exporter/main.go
+++ cmd/frr-exporter/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -46,6 +51,8 @@ func main() {
 	}
 	reg.MustRegister(collector)
 
+	ctx, cancel := context.WithCancel(context.Background())
+
 	// Expose the registered metrics via HTTP.
 	http.Handle("/metrics", promhttp.HandlerFor(
 		reg,
@@ -59,6 +66,45 @@ func main() {
 		Addr:              addr,
 		ReadHeaderTimeout: twenty * time.Second,
 		ReadTimeout:       time.Minute,
+		BaseContext:       func(_ net.Listener) context.Context { return ctx },
 	}
-	log.Fatal(server.ListenAndServe())
+	// Run server
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			// it is fine to use Fatal here because it is not main gorutine
+			log.Fatalf("HTTP server ListenAndServe: %v", err)
+		}
+	}()
+	signalChan := make(chan os.Signal, 1)
+
+	signal.Notify(
+		signalChan,
+		syscall.SIGHUP,  // kill -SIGHUP XXXX
+		syscall.SIGINT,  // kill -SIGINT XXXX or Ctrl+c
+		syscall.SIGQUIT, // kill -SIGQUIT XXXX
+	)
+	<-signalChan
+	log.Print("os.Interrupt - shutting down...\n")
+
+	go func() {
+		<-signalChan
+		log.Fatal("os.Kill - terminating...\n")
+	}()
+
+	gracefullCtx, cancelShutdown := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelShutdown()
+
+	if err := server.Shutdown(gracefullCtx); err != nil {
+		log.Printf("shutdown error: %v\n", err)
+		defer os.Exit(1)
+		return
+	} else {
+		log.Printf("gracefully stopped\n")
+	}
+
+	// manually cancel context if not using httpServer.RegisterOnShutdown(cancel)
+	cancel()
+
+	defer os.Exit(0)
+	return
 }
