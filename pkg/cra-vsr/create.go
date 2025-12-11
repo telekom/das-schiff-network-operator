fd5e6bdaff4a280b599d047796c7627e11e609d9
Romain Delhomel <romain.delhomel@6wind.com>
2025-12-11 12:52:27 +0100

    Setup cra-vsr gre, gretap and loopback interfaces
    Parse GREs and Loopbacks maps in nodenetworkconfig VRFs and configure
    these interfaces in the VSR.
    
    Signed-off-by: Romain Delhomel <romain.delhomel@6wind.com>

--- pkg/cra-vsr/create.go
+++ pkg/cra-vsr/create.go
@@ -66,7 +66,7 @@ func (m *Manager) generateUnderlayMAC() string {
 	return macAddr
 }
 
-func (Manager) createIPAddress(addr string, ipv4, ipv6 *IPAddressList) *IPAddress {
+func (Manager) createIPAddress(addr string, ipv4, ipv6 *IPAddressList) {
 	ipList := ipv6
 	if isIPv4(addr) {
 		ipList = ipv4
@@ -74,7 +74,6 @@ func (Manager) createIPAddress(addr string, ipv4, ipv6 *IPAddressList) *IPAddres
 	ipList.IPAddresses = append(ipList.IPAddresses, IPAddress{
 		IP: addr,
 	})
-	return &ipList.IPAddresses[len(ipList.IPAddresses)-1]
 }
 
 func (Manager) createVRF(name string, table int, ns *Namespace) *VRF {
