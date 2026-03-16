6b9176ef84be29aedd912b82a63408d70dcb619d
Christopher Dziomba <christopher.dziomba@telekom.de>
2026-03-16 10:03:20 +0100

    feat(e2e): update fabric topology for cluster-2
    - Remove m2mgw/c2mgw containerlab containers (replaced by cluster-2 pods)
    - Add nwop2-worker as ext-container with leaf1/leaf2 eth-srv4 links
    - Remove m2m/c2m VRF BGP sections from DCGWs (only mgmt VRF remains)
    - Remove loopback ip addr from DCGW setup.sh (FRR handles it)
    - Add leaf1/leaf2 eth-srv4 peer-group server neighbor

--- e2e/clab/frr/dcgw2/setup.sh
+++ e2e/clab/frr/dcgw2/setup.sh
@@ -5,8 +5,6 @@ set -euo pipefail
 sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv6.conf.all.forwarding=1
 
-ip addr add 192.0.2.2/32 dev lo
-
 # VRF mgmt
 ip link add vr.mgmt type vrf table 100
 ip link add vx.mgmt type vxlan id 20 dev lo local 192.0.2.2 dstport 4789 nolearning
@@ -16,36 +14,6 @@ ip link set br.mgmt master vr.mgmt
 ip link set br.mgmt up
 ip link set vx.mgmt up
 ip link set vr.mgmt up
-
-# VRF m2m
-ip link add vr.m2m type vrf table 2002026
-ip link add vx.m2m type vxlan id 2002026 dev lo local 192.0.2.2 dstport 4789 nolearning
-ip link add br.m2m type bridge
-ip link set vx.m2m master br.m2m
-ip link set br.m2m master vr.m2m
-ip link set m2mgw master vr.m2m
-ip link set br.m2m up
-ip link set vx.m2m up
-ip link set vr.m2m up
-ip addr add fdff:965f:f02a:2::/127 dev m2mgw
-
-# VRF c2m
-ip link add vr.c2m type vrf table 2002027
-ip link add vx.c2m type vxlan id 2002027 dev lo local 192.0.2.2 dstport 4789 nolearning
-ip link add br.c2m type bridge
-ip link set vx.c2m master br.c2m
-ip link set br.c2m master vr.c2m
-ip link set c2mgw master vr.c2m
-ip link set br.c2m up
-ip link set vx.c2m up
-ip link set vr.c2m up
-ip addr add fde9:9ec5:829e:2::/127 dev c2mgw
-
-# Static routes to m2mgw/c2mgw
-ip -4 route add 10.102.0.0/24 via inet6 fdff:965f:f02a:2::1 dev m2mgw vrf vr.m2m
-ip -6 route add fda5:25c1:193c::/64 via inet6 fdff:965f:f02a:2::1 dev m2mgw vrf vr.m2m
-ip -4 route add 10.102.1.0/24 via inet6 fde9:9ec5:829e:2::1 dev c2mgw vrf vr.c2m
-ip -6 route add fda5:25c1:193d::/64 via inet6 fde9:9ec5:829e:2::1 dev c2mgw vrf vr.c2m
 
 # NAT64 gateway
 ip link set eth-nat64 master vr.mgmt
