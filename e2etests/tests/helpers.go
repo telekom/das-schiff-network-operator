2bf312a9720004729dbf5d58bdfc99d7efe4659e
Maximilian Rink <maximilian.rink@telekom.de>
2026-06-08 18:33:25 +0200

    test: repair macvlan IPv6 DAD failures
    Signed-off-by: Maximilian Rink <maximilian.rink@telekom.de>

--- e2etests/tests/helpers.go
+++ e2etests/tests/helpers.go
@@ -1,6 +1,12 @@
 package tests
 
-import "os"
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/telekom/das-schiff-network-operator/e2etests/framework"
+)
 
 // readTestdata reads a file from the testdata directory.
 // When running inside the tester container, testdata is at /e2etests/testdata/.
@@ -13,4 +19,8 @@ func readTestdata(path string) ([]byte, error) {
 	}
 	// Fall back to relative path (CWD = e2etests/ when running go test)
 	return os.ReadFile("testdata/" + path)
+}
+
+func waitForNet1IPv6Ready(ctx context.Context, f *framework.Framework, namespace, podName, ipv6Addr string) error {
+	return f.WaitForIPv6DADComplete(ctx, namespace, podName, ipv6Addr, "net1", 30*time.Second)
 }
