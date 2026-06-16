df1230dfc45deec505d016507a6cde90c2e6532d
Maximilian Rink <maximilian.rink@telekom.de>
2026-06-16 16:12:08 +0200

    test: fail fast for invalid IPv6 DAD targets

--- e2etests/framework/dad_test.go
+++ e2etests/framework/dad_test.go
@@ -1,6 +1,47 @@
 package framework
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
+
+func TestParseIPv6Target(t *testing.T) {
+	tests := []struct {
+		name    string
+		addr    string
+		wantErr string
+	}{
+		{
+			name: "valid IPv6",
+			addr: "fd94:685b:30cf:501::10",
+		},
+		{
+			name:    "invalid target",
+			addr:    "not-an-ip",
+			wantErr: "invalid IPv6 address",
+		},
+		{
+			name:    "IPv4 target",
+			addr:    "192.0.2.10",
+			wantErr: "is not IPv6",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := parseIPv6Target(tt.addr)
+			if tt.wantErr == "" {
+				if err != nil {
+					t.Fatalf("parseIPv6Target() error = %v, want nil", err)
+				}
+				return
+			}
+			if err == nil || !strings.Contains(err.Error(), tt.wantErr) {
+				t.Fatalf("parseIPv6Target() error = %v, want containing %q", err, tt.wantErr)
+			}
+		})
+	}
+}
 
 func TestParseIPv6DADState(t *testing.T) {
 	tests := []struct {
@@ -60,20 +101,15 @@ func TestParseIPv6DADState(t *testing.T) {
 			wantState: ipv6DADReady,
 			wantFound: true,
 		},
-		{
-			name: "invalid target",
-			output: `5: net1@if11: <BROADCAST,MULTICAST,UP,LOWER_UP,M-DOWN> mtu 1500 qlen 1000
-    inet6 fd94:685b:30cf:501::10/64 scope global
-       valid_lft forever preferred_lft forever`,
-			addr:      "not-an-ip",
-			wantState: ipv6DADReady,
-			wantFound: false,
-		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotCIDR, gotState, gotFound := parseIPv6DADState(tt.output, tt.addr)
+			target, err := parseIPv6Target(tt.addr)
+			if err != nil {
+				t.Fatalf("parseIPv6Target() error = %v", err)
+			}
+			gotCIDR, gotState, gotFound := parseIPv6DADState(tt.output, target)
 			if gotFound != tt.wantFound {
 				t.Fatalf("found = %v, want %v", gotFound, tt.wantFound)
 			}
